905bc2bd9786d282db0b472b1b8bf701721ac311
Козюра Алексей Андреевич <kozyura_aleksey@mail.ru>
2020-01-12 05:17:31 +0300

    rules

--- View/Menu/ViewMenu.cs
+++ View/Menu/ViewMenu.cs
@@ -39,9 +39,13 @@ namespace View.Menu
         /// <param name="parItem"></param>
         private void DrawMenuItem(MenuItems parItem)
         {
-            _bufer.Graphics.DrawString("Game rules:", new Font("Calibri", 18), new SolidBrush(Color.White), 10, View.viewform.ClientRectangle.Height-130);
-            _bufer.Graphics.DrawString("Press Enter to Start Game", new Font("Calibri", 18), new SolidBrush(Color.White), 230, View.viewform.ClientRectangle.Height-160);
-            _bufer.Graphics.DrawString("Press Esc to Exit", new Font("Calibri", 18), new SolidBrush(Color.White), 230, View.viewform.ClientRectangle.Height-130);
+            _bufer.Graphics.DrawString("Game rules:", new Font("Calibri", 18), new SolidBrush(Color.White), 10, View.viewform.ClientRectangle.Height - 470);
+            _bufer.Graphics.DrawLine(new Pen(new SolidBrush(Color.White)), 0, 70, View.viewform.ClientRectangle.Height, 70);
+            _bufer.Graphics.DrawString("Collect all the gold on the map by moving the keys: 'up', ", new Font("Calibri", 15), new SolidBrush(Color.White), 10, View.viewform.ClientRectangle.Height - 440);
+            _bufer.Graphics.DrawString("'down', 'right', 'left'.", new Font("Calibri", 15), new SolidBrush(Color.White), 10, View.viewform.ClientRectangle.Height - 410);
+            _bufer.Graphics.DrawString("You can also move along the ropes", new Font("Calibri", 15), new SolidBrush(Color.White), 10, View.viewform.ClientRectangle.Height - 380);
+            _bufer.Graphics.DrawString("Press Enter to Start Game", new Font("Calibri", 18), new SolidBrush(Color.White), 10, View.viewform.ClientRectangle.Height - 300);
+            _bufer.Graphics.DrawString("Press Esc to Exit", new Font("Calibri", 18), new SolidBrush(Color.White), 10, View.viewform.ClientRectangle.Height - 270);
         }
 
         /// <summary>
@@ -49,8 +53,8 @@ namespace View.Menu
         /// </summary>
         private void DrawTitle()
         {
-            _bufer.Graphics.DrawString("LodeRunner", new Font("Calibri", 20), new SolidBrush(Color.White) , 10 , View.viewform.ClientRectangle.Height - 160);
-            //Canvas.DrawLine(new Pen(new SolidBrush(Color.White)), 0, 40, View.viewform.ClientRectangle.Height, 40);
+            _bufer.Graphics.DrawString("LodeRunner", new Font("Calibri", 20), new SolidBrush(Color.White) , 170 , View.viewform.ClientRectangle.Height - 500);
+            _bufer.Graphics.DrawLine(new Pen(new SolidBrush(Color.White)), 0, 40, View.viewform.ClientRectangle.Height, 40);
         }
 
         /// <summary>
